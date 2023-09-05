3a7f2f7db838bb3c9bc3c29b3fde579c087f0733
JIMMON_JOY <jimmon88@gmail.com>
2023-09-05 10:54:22 -0400

    browser refresh issue fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { TriviaComponent } from './trivia/trivia.component';
 import { TriviaAnswersComponent } from './trivia-answers/trivia-answers.component';
 import { HttpClientModule } from '@angular/common/http';
-import { from } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, TriviaComponent, TriviaAnswersComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [],
+  providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
